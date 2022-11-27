069f90731ccfddc8c80b8e2dae392bc227a6e225
counter666 <69075185+counter666@users.noreply.github.com>
2022-11-27 09:56:21 +0800

    Update blocks.py

--- models/blocks.py
+++ models/blocks.py
@@ -1,1 +1,287 @@
+from Blocks import *
+import torch.nn.init as init
+import torch.nn.functional as F
+import pdb
+import math
+import torch
+import torch.nn as nn
+from collections import OrderedDict
+from torch.nn import Parameter
 
+
+def convBlock(nin, nout, kernel_size=3, batchNorm=False, layer=nn.Conv2d, bias=True, dropout_rate=0.0, dilation=1):
+    if batchNorm == False:
+        return nn.Sequential(
+            nn.PReLU(), 
+            nn.Dropout(p=dropout_rate),
+            layer(nin, nout, kernel_size=kernel_size, bias=bias, dilation=dilation) 
+        )
+    else:
+        return nn.Sequential(
+            nn.BatchNorm2d(nin),
+            nn.PReLU(),
+            nn.Dropout(p=dropout_rate),
+            layer(nin, nout, kernel_size=kernel_size, bias=bias, dilation=dilation)
+        )
+
+
+def convBatch(nin, nout, kernel_size=3, stride=1, padding=1, bias=False, layer=nn.Conv2d, dilation=1):
+    return nn.Sequential(
+        layer(nin, nout, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias, dilation=dilation),
+        nn.BatchNorm2d(nout),
+        # nn.LeakyReLU(0.2)
+        nn.PReLU()
+    )
+
+def conv3x3(in_channels, out_channels, module_name, postfix,
+            stride=1, groups=1, kernel_size=3, padding=1):
+    """3x3 convolution with padding"""
+    return [
+        ('{}_{}/conv'.format(module_name, postfix),
+            nn.Conv2d(in_channels, out_channels,
+                      kernel_size=kernel_size,
+                      stride=stride,
+                      padding=padding,
+                      groups=groups,
+                      bias=False)),
+        ('{}_{}/norm'.format(module_name, postfix),
+            nn.BatchNorm2d(out_channels)),
+        ('{}_{}/relu'.format(module_name, postfix),
+            nn.ReLU(inplace=True)),
+    ]
+
+
+def conv1x1(in_channels, out_channels, module_name, postfix,
+            stride=1, groups=1, kernel_size=1, padding=0):
+    """1x1 convolution"""
+    return [
+        ('{}_{}/conv'.format(module_name, postfix),
+            nn.Conv2d(in_channels, out_channels,
+                      kernel_size=kernel_size,
+                      stride=stride,
+                      padding=padding,
+                      groups=groups,
+                      bias=False)),
+        ('{}_{}/norm'.format(module_name, postfix),
+            nn.BatchNorm2d(out_channels)),
+        ('{}_{}/relu'.format(module_name, postfix),
+            nn.ReLU(inplace=True)),
+    ]
+
+class InConv_t(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(InConv_t, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True)
+        )
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+class InConv_b(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(InConv_b, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, kernel_size=2, stride=2, padding=1, dilation=2),
+            nn.Conv2d(out_ch, out_ch, kernel_size=5, padding=2),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+class OutConv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_ch, out_ch, 1)
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+class Down(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(Down, self).__init__()
+        self.mpconv = nn.Sequential(
+            # nn.MaxPool2d(2),
+            nn.Conv2d(in_ch, out_ch, kernel_size=2, stride=2, padding=1, dilation=2)
+            # nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1)
+        )
+
+    def forward(self, x):
+        x = self.mpconv(x)
+        return x
+
+
+class h_sigmoid(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+
+    def forward(self, x):
+        return self.relu(x + 3) / 6
+
+
+class h_swish(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_swish, self).__init__()
+        self.sigmoid = h_sigmoid(inplace=inplace)
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
+
+
+class CoordAtt(nn.Module):
+    def __init__(self, inp, oup, reduction=32):
+        super(CoordAtt, self).__init__()
+        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
+        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
+
+        mip = max(8, inp // reduction)
+
+        self.conv1 = nn.Conv2d(inp, mip, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(mip)
+        self.act = h_swish()
+
+        self.conv_h = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
+        self.conv_w = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x):
+        identity = x
+
+        n, c, h, w = x.size()
+        x_h = self.pool_h(x)
+        x_w = self.pool_w(x).permute(0, 1, 3, 2)
+       
+        y = torch.cat([x_h, x_w], dim=2)
+     
+        y = self.conv1(y)
+        y = self.bn1(y)
+        y = self.act(y)
+
+        x_h, x_w = torch.split(y, [h, w], dim=2)
+        x_w = x_w.permute(0, 1, 3, 2)
+        a_h = self.conv_h(x_h).sigmoid()
+        a_w = self.conv_w(x_w).sigmoid()
+
+        out = identity * a_w * a_h
+      
+        return out
+
+
+
+
+class Res_Bottleneck(nn.Module):
+
+    expansion = 4
+
+    def __init__(self, in_channel, out_channel, stride=1, downsample=None):
+        super(Res_Bottleneck, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=out_channel,
+                               kernel_size=1, stride=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channel)
+
+        self.conv2 = nn.Conv2d(in_channels=out_channel, out_channels=out_channel,
+                               kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channel)
+
+        self.conv3 = nn.Conv2d(in_channels=out_channel, out_channels=out_channel * self.expansion,
+                               kernel_size=1, stride=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_channel * self.expansion)
+
+        self.relu = nn.ReLU(inplace=True)
+
+        self.downsample = downsample
+
+    def forward(self, x):
+        identity = x
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+      
+class Up(nn.Module):
+    def __init__(self, in_ch, out_ch, bilinear=True):
+        super(Up, self).__init__()
+
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        else:
+            self.up = nn.ConvTranspose2d(in_ch // 2, in_ch // 2, 2, stride=2)
+
+        self.conv = nn.Conv2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1)
+        self.coorCA = CoordAtt(out_ch, out_ch)
+
+    def forward(self, x1, x2, x3):
+        x1 = self.up(x1)
+        # print('x1.shape:', x1.shape)
+
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, (diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2))
+        x = torch.cat([x1, x2, x3], dim=1)
+        x = self.conv(x)
+        x = self.coorCA(x)
+
+        return x
+
+class DSAB_block(nn.Module):
+    def __init__(self,
+                 in_ch,  
+                 stage_ch, 
+                 concat_ch, 
+                 layer_per_block,
+                 module_name,  
+                 identity=False):
+        super(DSAB_block, self).__init__()
+
+        self.identity = identity
+        self.layers = nn.ModuleList()
+        in_channel = in_ch
+        for i in range(layer_per_block):
+            self.layers.append(nn.Sequential(
+                OrderedDict(conv3x3(in_channel, stage_ch, module_name, i))))
+            in_channel = stage_ch 
+
+        # feature aggregation
+        in_channel = in_ch + layer_per_block * stage_ch 
+        self.concat = nn.Sequential(
+            OrderedDict(conv1x1(in_channel, concat_ch, module_name, 'concat'))) 
+
+    def forward(self, x):
+        identity_feat = x
+        output = []
+        output.append(x)
+        for layer in self.layers:
+            x = layer(x)
+            output.append(x)
+
+        x = torch.cat(output, dim=1)
+        xt = self.concat(x)
+
+        if self.identity:
+            xt = xt + identity_feat
+
+        return xt
