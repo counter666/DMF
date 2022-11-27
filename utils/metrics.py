974ccfb9fd3d1d7b655b6e6bacd9e1093a02b7b2
counter666 <69075185+counter666@users.noreply.github.com>
2022-11-27 11:02:40 +0800

    Create metrics.py

---
+++ utils/metrics.py
@@ -0,0 +1,82 @@
+def dc(result, reference):
+    result = np.atleast_1d(result.astype(np.bool))  
+    reference = np.atleast_1d(reference.astype(np.bool))  
+
+    tp = np.count_nonzero(result & reference)
+    fp = np.count_nonzero(result & ~reference)
+    tn = np.count_nonzero(~result & ~reference)
+    fn = np.count_nonzero(~result & reference)
+
+    try:
+        dc = 2.0 * tp / float(fn+fp+2*tp)
+    except ZeroDivisionError:
+        dc = 0.0
+
+    return dc
+  
+def jc(result, reference):
+   
+    result = np.atleast_1d(result.astype(np.bool))
+    reference = np.atleast_1d(reference.astype(np.bool))
+    
+    tp = np.count_nonzero(result & reference)
+    fp = np.count_nonzero(result & ~reference)
+    tn = np.count_nonzero(~result & ~reference)
+    fn = np.count_nonzero(~result & reference)
+
+    jc = float(tp) / (float(fp+fn+tp)+0.000001)
+
+    return jc
+  
+def recall(result, reference):
+    
+    result = np.atleast_1d(result.astype(np.bool))
+    reference = np.atleast_1d(reference.astype(np.bool))
+
+    tp = np.count_nonzero(result & reference)
+    fn = np.count_nonzero(~result & reference)
+
+    try:
+        recall = tp / float(tp + fn)
+    except ZeroDivisionError:
+        recall = 0.0
+
+    return recall
+
+
+def sensitivity(result, reference):
+   
+    return recall(result, reference)
+
+
+def specificity(result, reference):
+    
+    result = np.atleast_1d(result.astype(np.bool))
+    reference = np.atleast_1d(reference.astype(np.bool))
+
+    tn = np.count_nonzero(~result & ~reference)
+    fp = np.count_nonzero(result & ~reference)
+
+    try:
+        specificity = tn / float(tn + fp)
+    except ZeroDivisionError:
+        specificity = 0.0
+
+    return specificity
+
+  
+def accuracy(result, reference):
+    result = np.atleast_1d(result.astype(np.bool))
+    reference = np.atleast_1d(reference.astype(np.bool))
+
+    tp = np.count_nonzero(result & reference)
+    fp = np.count_nonzero(result & ~reference)
+    tn = np.count_nonzero(~result & ~reference)
+    fn = np.count_nonzero(~result & reference)
+
+    try:
+        accuracy = (tp + tn) / float(tp + fn + tn + fp)
+    except ZeroDivisionError:
+        accuracy = 0.0
+
+    return accuracy
